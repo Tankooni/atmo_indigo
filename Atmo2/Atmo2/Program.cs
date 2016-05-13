7399da18e78696d6688c95bd4a6a17c9a4454e4c
Zack Wheeler <913966+Tankooni@users.noreply.github.com>
2016-05-13 01:39:25 -0400

    First commit. Contains basic Indigo project with some of my utility classes. Brought over content from Atmo1.

---
+++ Atmo2/Atmo2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Indigo;
+using Utility;
+using Atmo2.Worlds;
+
+namespace Atmo2
+{
+	class Game : Engine
+	{
+		static void Main(string[] args)
+		{
+			var game = new Game();
+			game.Run();
+		}
+
+		public Game() :
+			base(1280, 720, 60)
+		{
+			FP.Screen.Title = "Atmo2";
+			FP.Console.Enable();
+			FP.Console.MirrorToSystemOut = true;
+			FP.Console.ToggleKey = Indigo.Inputs.Keyboard.Tilde;
+
+			Indigo.Inputs.Mouse.CursorVisible = true;
+			//AudioManager.Init(1);
+			//AudioManager.PlayMusic("B_piano_mid");
+			FP.World = new GameWorld();
+		}
+	}
+}
---
+++ Atmo2/Atmo2/utils/Utility.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utility
+{
+	class Utility
+	{
+		public static string CurrentMusic { get; internal set; }
+		public static float MusicVolume = 1;
+
+		/// <summary>
+		/// Returns all files in a folder using one or more search filters.
+		/// Treats filters as or's
+		/// </summary>
+		/// <param name="path">Path to folder to search</param>
+		/// <param name="filters">A list of filters speparated by |</param>
+		/// <returns></returns>
+		public static string[] RetrieveFilePathForFilesInDirectory(string path, string filters)
+		{
+			List<string> files = new List<string>();
+			foreach (string filter in filters.Split('|'))
+				files.AddRange(Directory.GetFiles(path, filter));
+			return files.Select(x => x.Remove(0, 2).Replace(@"\", "/")).ToArray();
+		}
+
+		public static List<Type> GetTypeFromAllAssemblies<T>()
+		{
+			var typeList = new List<Type>();
+			var assemblies = AppDomain.CurrentDomain.GetAssemblies();
+			for (int i = 0; i < assemblies.Length; ++i)
+			{
+				var types = assemblies[i].GetTypes();
+				for (int j = 0; j < types.Length; ++j)
+				{
+					var t = types[j];
+					if (typeof(T).IsAssignableFrom(t))
+						typeList.Add(t);
+				}
+			}
+
+			return typeList;
+		}
+	}
+}
