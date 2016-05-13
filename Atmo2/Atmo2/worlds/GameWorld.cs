722a0497b2edf37a8199930ce6f2e5d286d8580a
Tankooni <Tankooni@localhost>
2016-05-13 01:39:25 -0400

    First commit. Contains basic Indigo project with some of my utility classes. Brought over content from Atmo1.

---
+++ Atmo2/Atmo2/worlds/GameWorld.cs
@@ -0,0 +1,19 @@
+﻿using Indigo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Utility;
+
+namespace Atmo2.Worlds
+{
+	public class GameWorld : World
+	{
+		public override void Update()
+		{
+			base.Update();
+			Console.WriteLine(AudioManager.CurrentSong.Position);
+		}
+	}
+}
