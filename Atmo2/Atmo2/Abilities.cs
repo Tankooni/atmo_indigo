7756cf43490cada8ec8a3a0ba77efa72a890a9a6
Lindenk <Lindenk@localhost>
2017-01-22 15:49:32 -0500

    Added a divekick and abilities are locked at the start.

--- Atmo2/Atmo2/Abilities.cs
+++ Atmo2/Atmo2/Abilities.cs
@@ -3,20 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Atmo2.Entities;
 
 namespace Atmo2
 {
 	public class Abilities
 	{
-		public bool GoodJump { get; set; }
+        private const float SHITTY_JUMP_STRENTH = 4f;
+        private const float GOOD_JUMP_STRENTH = 8f;
+		public bool GoodJump
+        {
+            get { return player.JumpStrenth == GOOD_JUMP_STRENTH; }
+            set
+            {
+                player.JumpStrenth = value ?
+                  GOOD_JUMP_STRENTH :
+                  SHITTY_JUMP_STRENTH;
+            }
+        }
 		public bool DoubleJump { get; set; }
 		public bool WallSlide { get; set; }
 		public bool WallJump { get; set; }
 		public bool GroundDash { get; set; }
 		public bool AirDash { get; set; }
 
-		public Abilities()
+        private Player player;
+		public Abilities(Player player)
 		{
+            this.player = player;
 			GoodJump = false;
 			DoubleJump = false;
 			WallSlide = false;
