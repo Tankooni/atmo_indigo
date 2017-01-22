1acdbf0823e975b98ec7c0915ffdec2dd5745402
Zack Wheeler <913966+Tankooni@users.noreply.github.com>
2017-01-22 14:38:12 -0500

    Started adding pickups. Fixed door sprite. Removed bad always adding to list that was causing slight leak. Added new version of music loop

---
+++ Atmo2/Atmo2/Entities/Pickup.cs
@@ -0,0 +1,22 @@
+﻿using Indigo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atmo2.Entities
+{
+	public enum PickupType
+	{
+		Jump,
+		AirJump,
+		Dash,
+		AirDash
+	}
+
+	public class Pickup : Entity
+	{
+
+	}
+}
